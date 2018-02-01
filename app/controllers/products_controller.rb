8f3f115ba9cd45e75181aadb55e53b662d61c532
Alex <alexchangbz@gmail.com>
2018-02-01 08:17:44 +0000

    Fix create action by adding proper flash msg and add show action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,16 @@ class ProductsController < ApplicationController
   
   def create
     @product = Product.new(product_params)
-    @product.save
-    redirect_to article_show(@product)
+    if @product.save
+      flash[:notice] = "Product has been successfully added"
+      redirect_to product_path(@product)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @product = Product.find(params[:id])
   end
   
   private
