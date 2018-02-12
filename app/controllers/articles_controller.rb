631b108b0e2dfe693752a35a6967b05a5bb524fa
Alex <alexchangbz@gmail.com>
2018-02-12 05:24:50 +0000

    Update article user to be the creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-    @article.user = User.last
+    @article.user = current_user
     if @article.save
       flash[:success] = "Your article has been successfully published"
       redirect_to article_path(@article)
